c7a16f221d479052946abcbf8cac37ec307e1e3b
kavinsurya <kavinsurya002@gmail.com>
2021-02-24 12:09:55 +0530

    Mail component

--- src/Mail.js
+++ src/Mail.js
@@ -1,15 +1,64 @@
+import { IconButton } from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./Mail.css";
 
-
-
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 function Mail() {
+  const history = useHistory();
   return (
     <div className="mail">
-    
+      <div className="mail_tools">
+        <div className="mail_toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail--toolsRight"></div>
       </div>
-    
+      <h1>Kavin</h1>
+    </div>
   );
 }
 
